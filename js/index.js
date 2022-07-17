b63c6e5198dd69c297e48835e02811e2b1394a91
Eric Rao <eric.rao1460@gmail.com>
2022-07-16 21:37:28 -0300

    aplicando manipulación del DOM

--- js/index.js
+++ js/index.js
@@ -1,30 +1,75 @@
-let productos = [
-    {id: 1, nombre: "Playstation 5", precio: 100000,  img: '/img/buzo.jpeg'},
-    {id: 2, nombre: "Pelota de Futbol", precio: 1500, img: '/img/buzo.jpeg'},
-    {id: 3, nombre: "Paleta de padel", precio: 45000, img: '/img/buzo.jpeg'},
-    {id: 4, nombre: "Nintendo switch", precio: 56000, img: '/img/buzo.jpeg'}
-]
-
 let prodCarrito = []
+let cantCarrito = document.getElementById("cartQty")
+let totalCarrito = document.getElementById("cartTotal")
+let contenedorProductos = document.getElementById("contenedorProductos")
+let listaCarrito = document.getElementById("listaCarrito")
+console.log(listaCarrito)
 
-function agregarProducto() {
-    let agregarProdUsuario = prompt("Que producto queres agregar?").toLowerCase()
-    console.log(agregarProdUsuario)
+productosDisponibles()
 
-    let prodEncontrado = productos.find((el) => el.nombre.toLowerCase() == agregarProdUsuario)
-    console.log(prodEncontrado)
-    if (prodEncontrado) {
-        prodCarrito.push(prodEncontrado)
-        alert(`producto agregado`)
-    } else {
-        alert(`No existe ese producto`)
+function productosDisponibles() {
+    let div2 = document.createElement('div')
+    div2.className = 'row d-flex justify-content-center'
+    productos.forEach((prod) => {
+        let div = document.createElement('div')
+        div.className = 'col-md-3 card__border'
+        div.innerHTML = `<div class="containerimg">
+                            <img src="${prod.img}" class="card-img-top" alt="${prod.alt}">
+                        </div>    
+                        <div class="card-body ">
+                            <h5 class="card-title text-center">${prod.nombre}</h5>
+                            <p class="card-text text-center">$${prod.precioString}</p>
+                            <button class="btn btn-primary btn-comprar" id="buyButton">Comprar</button>
+                        </div>`
+                        
+        div2.append(div)                
+        contenedorProductos.appendChild(div2)    
+        console.log(prod)            
+    } )
+}
+
+function actualizarTotal() {
+    let total = prodCarrito.reduce((acc, el) => acc + el.precio, 0)
+    totalCarrito.textContent = `Total: $${total}`
+}
+
+function actualizarCantCarrito() {
+    cantCarrito.textContent = prodCarrito.length
+}
+
+const buyButton = document.getElementById("contenedorProductos")
+buyButton.addEventListener("click", (e) => {
+    addCart(e)
+})
+const addCart = ((e) => {
+    console.log(e.target.classList.contains("btn-comprar"))
+    if (e.target.classList.contains("btn-comprar")) {
+        
+        setCarrito(e.target.parentElement)
     }
-}
+    
+})
 
-function verCarrito() {
+function productosCarrito() {
+    listaCarrito.innerHTML = ''
+    prodCarrito.forEach((product) => {
+        let contenido = document.createElement('div')
+        contenido.innerHTML = `<li class="px-2 d-flex justify-content-between align-items-start item" id="listaCarrito">${product.nombre} <div class='precio'>${product.precioString}</li>`
+        listaCarrito.append(contenido)    
+    })
+}
+const setCarrito = (prod) => {
+    const productos = {
+        nombre: prod.querySelector('h5').textContent,
+        precio: Number(prod.querySelector('p').textContent.replace('$', '').replace('.', '')) ,
+        precioString: prod.querySelector('p').textContent,
+        cantidad: 1
+    }
+
+    console.log(productos)
+    prodCarrito.push(productos)
     console.log(prodCarrito)
-}
-
-function pagarTotal() {
-    alert(`Total a pagar es de $ ${prodCarrito.reduce((acc, el) => acc + el.precio, 0)}`)
+    actualizarCantCarrito()
+    productosCarrito()
+    actualizarTotal()
 }
---
+++ js/products.js
@@ -0,0 +1,9 @@
+let productos = [
+    {id: 1, nombre: "Playstation 5", precio: 100000, precioString: '100.000',  img: './assets/play5.jpg', alt: "play 5"},
+    {id: 2, nombre: "Pelota de Futbol", precio: 1500, precioString: '1.500', img: './assets/pelotafutbol.jpg', alt: "pelota futbol"},
+    {id: 3, nombre: "Paleta de padel", precio: 45000, precioString: '45.000', img: './assets/paletapadel.jpg', alt: "paleta padel"},
+    {id: 4, nombre: "Nintendo switch", precio: 56000, precioString: '56.000', img: './assets/switch.jpg', alt: "switch"},
+    {id: 5, nombre: "Auriculares", precio: 21999, precioString: '21.999', img: './assets/auriculares.webp', alt: "headphones"},
+    {id: 6, nombre: "Iphone", precio: 469499, precioString: '469.499', img: './assets/iphone.jpg', alt: "iphone"}
+
+]
